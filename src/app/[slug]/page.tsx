9a6a75fa0ddca681c5d297f9ddd8dab3f462ccf5
caiyingshen <caiying.shen526@gmail.com>
2024-12-26 23:41:38 -0500

    fix vercel build errors

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { PortableText, type SanityDocument } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
@@ -6,6 +7,7 @@ import Link from "next/link";
 import { portfolioEntryQuery, updatesQuery } from "@/static/queries";
 import "../../css/postPage.css";
 import "../../css/base.css";
+import { useEffect, useState } from "react";
 
 const { projectId, dataset } = client.config();
 const urlFor = (source: SanityImageSource) =>
@@ -15,80 +17,114 @@ const urlFor = (source: SanityImageSource) =>
 
 const options = { next: { revalidate: 30 } };
 
-export default async function PostPage({
-  params,
-}: {
-  params: { slug: string };
-}) {
-  const post = await client.fetch<SanityDocument>(
-    portfolioEntryQuery,
-    params,
-    options
-  );
-  const postImageUrl = post.image ? urlFor(post.image)?.url() : null;
+export default function PostPage({ params }: { params: Promise<{ slug: string }> }) {
+  const [post, setPost] = useState<SanityDocument>();
+  const [pieceUpdates, setPieceUpdates] = useState<SanityDocument>();
+  const [postImageUrl, setPostImageUrl] = useState<string>();
 
-  const pieceIdRefs = post.pieceIds.map((p: { _ref: string }) => p._ref);
-  const pieceUpdates = await client.fetch<SanityDocument>(
-    updatesQuery,
-    { pieceIdArray: pieceIdRefs },
-    options
-  );
+  async function getPost() {
+    const post = await client.fetch<SanityDocument>(
+      portfolioEntryQuery,
+      params,
+      options
+    );
+    setPost(post);
+    if (post.image) {
+      setPostImageUrl(urlFor(post.image)?.url());
+    }
+  }
+
+  async function getPieceUpdates() {
+    const pieceIdRefs = post?.pieceIds.map((p: { _ref: string }) => p._ref);
+    const pieceUpdates = await client.fetch<SanityDocument>(
+      updatesQuery,
+      { pieceIdArray: pieceIdRefs },
+      options
+    );
+
+    setPieceUpdates(pieceUpdates);
+  }
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  useEffect(() => {
+    if (post) {
+      getPieceUpdates();
+    }
+  }, [post]);
 
   return (
-    <div className="post-container">
-      <div className="main-post">
-        <Link href="/" className="hover:underline back-arrow">
-          ← Back to posts
-        </Link>
-        {postImageUrl && (
-          <img className="post-image" src={postImageUrl} alt={post.title} />
-        )}
-      </div>
+    <>
+      {post && pieceUpdates ? (
+        <div className="post-container">
+          <div className="main-post">
+            <Link href="/" className="hover:underline back-arrow">
+              ← Back to posts
+            </Link>
+            {postImageUrl && (
+              <img className="post-image" src={postImageUrl} alt={post.title} />
+            )}
+          </div>
 
-      <div className="notes-section">
-        <h1 className="text-3xl font-bold mb-8">{post.title}</h1>
-        <div className="prose">
-          <p className="p1">
-            Published: {new Date(post.publishedAt).toLocaleDateString()}
-          </p>
-          {Array.isArray(post.body) && <PortableText value={post.body} />}
+          <div className="notes-section">
+            <h1 className="text-3xl font-bold mb-8">{post.title}</h1>
+            <div className="prose">
+              <p className="p1">
+                Published: {new Date(post.publishedAt).toLocaleDateString()}
+              </p>
+              {Array.isArray(post.body) && <PortableText value={post.body} />}
+            </div>
+          </div>
+
+          <div className="updates">
+            {pieceUpdates.length > 0 && (
+              <h3 className="text-2xl font-bold mb-8">Progress Updates</h3>
+            )}
+            {pieceUpdates.map(
+              (
+                up: {
+                  image: SanityImageSource;
+                  notes: string;
+                  updatedAt: string;
+                },
+                i: number
+              ) => {
+                const postImageUrl = up.image
+                  ? urlFor(up.image)?.width(200).height(200).url()
+                  : null;
+
+                return (
+                  <div className="update-container" key={`index-${i}`}>
+                    {postImageUrl && (
+                      <img
+                        className="update-image"
+                        src={postImageUrl}
+                        alt={up.notes}
+                      />
+                    )}
+
+                    <div className="update-notes">
+                      <p className="p1">
+                        {new Date(up.updatedAt).toLocaleDateString()}
+                      </p>
+                      <p> {up.notes}</p>
+                    </div>
+                  </div>
+                );
+              }
+            )}
+          </div>
         </div>
-      </div>
-
-      <div className="updates">
-        {pieceUpdates.length > 0 && (
-          <h3 className="text-2xl font-bold mb-8">Progress Updates</h3>
-        )}
-        {pieceUpdates.map(
-          (
-            up: { image: SanityImageSource; notes: string; updatedAt: string },
-            i: any
-          ) => {
-            const postImageUrl = up.image
-              ? urlFor(up.image)?.width(200).height(200).url()
-              : null;
-
-            return (
-              <div className="update-container" key={`index-${i}`}>
-                {postImageUrl && (
-                  <img
-                    className="update-image"
-                    src={postImageUrl}
-                    alt={up.notes}
-                  />
-                )}
-
-                <div className="update-notes">
-                  <p className="p1">
-                    {new Date(up.updatedAt).toLocaleDateString()}
-                  </p>
-                  <p> {up.notes}</p>
-                </div>
-              </div>
-            );
-          }
-        )}
-      </div>
-    </div>
+      ) : (
+        <img
+          className="loading-spinner"
+          src="https://i.gifer.com/ZKZg.gif"
+          height="25px"
+          width="25px"
+        />
+      )}
+    </>
   );
 }
--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -10,26 +10,26 @@ export default function AboutMePage() {
       </Link>
       <div className="bio-container">
         <h3 className="heading">about me</h3>
-        <p className="intro">Hi there! 👋 I'm Betty. Here's a bit about me: </p>
+        <p className="intro">{"Hi there! 👋 I'm Betty. Here's a bit about me: "}</p>
         <li className="bio">
-          👩🏻‍💻 I've been a software engineer for over 4 years now. I'm currently a
-          software engineer at Hightouch on the destinations team.
+          {`👩🏻‍💻 I've been a software engineer for over 4 years now. I'm currently a
+          software engineer at Hightouch on the destinations team.`}
         </li>
         <li className="bio">
-          🧠 I love designing and playing with elegant APIs, and creating
-          pleasant and seamless user experiences.
+          {`🧠 I love designing and playing with elegant APIs, and creating
+          pleasant and seamless user experiences.`}
         </li>
         <li className="bio">
-          🏺 I've been making pottery for almost a full year now! While I've
-          learned a lot, the list of pieces I want to make is endless.
+          {`🏺 I've been making pottery for almost a full year now! While I've
+          learned a lot, the list of pieces I want to make is endless.`}
         </li>
         <li className="bio">
-          ✨ Outside of coding and making pottery, you can find me training for
+          {`✨ Outside of coding and making pottery, you can find me training for
           a race, at the nearest [solidcore], traveling to a different
-          continent, or baking my newest recipe fixation.
+          continent, or baking my newest recipe fixation.`}
         </li>
         <li className="bio">
-          You can reach me at caiying.shen526@gmail.com, or any of the links below.
+          {`You can reach me at caiying.shen526@gmail.com, or any of the links below.`}
         </li>
       </div>
       <Footer />
